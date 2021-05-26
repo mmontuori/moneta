fb866f5e7302905db9b3592d30bf11bace8e0a98
mmontuori <michael.montuori@gmail.com>
2021-05-26 11:40:14 -0400

    fixed issue with initial block reward

--- src/validation.cpp
+++ src/validation.cpp
@@ -1151,7 +1151,10 @@ CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
     if (halvings >= 64)
         return 0;
 
-    CAmount nSubsidy = QUARK_PREMINED * COIN;
+    if (nHeight == 1) {
+        return QUARK_BLOCK_ONE_REWARD * COIN;
+    }
+    CAmount nSubsidy = QUARK_REWARD * COIN;
     // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
     nSubsidy >>= halvings;
     return nSubsidy;
