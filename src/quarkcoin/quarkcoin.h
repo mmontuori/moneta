0da4b9a66e9aea04bf8bf3573bc661d7c33fe0f4
mmontuori <michael.montuori@gmail.com>
2021-05-29 16:21:32 -0400

    work in progress

--- src/quarkcoin/quarkcoin.h
+++ src/quarkcoin/quarkcoin.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2011 Michael Montuori
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #define QUARK_REWARD 500
 #define QUARK_BLOCK_ONE_REWARD 1000000
 #define QUARK_BLOCK_REWARD_COUNT 5000
