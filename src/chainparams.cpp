cae22fc7bc819a7dadadd843d6a6a3b39a0f1fcb
mmontuori <michael.montuori@gmail.com>
2021-06-02 22:30:38 -0400

    working on issue #7

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -134,7 +134,7 @@ public:
         vSeeds.emplace_back("dnsseed.litecoinpool.org");
         vSeeds.emplace_back("dnsseed.koin-project.com");
 
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,48);
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,QUARK_PUBKEY_ADDRESS);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
         base58Prefixes[SCRIPT_ADDRESS2] = std::vector<unsigned char>(1,50);
         base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,176);
